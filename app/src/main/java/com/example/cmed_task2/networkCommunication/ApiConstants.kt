6da4f9286a09c92ed1e32a7e7cacf0b0e02742ba
omi-72 <omi@techetronventures.com>
2023-10-26 22:04:42 +0600

    add base url & end url class name by ApiConstents for api calling

---
+++ app/src/main/java/com/example/cmed_task2/networkCommunication/ApiConstants.kt
@@ -0,0 +1,8 @@
+package com.example.cmed_task2.networkCommunication
+
+class ApiConstants {
+    companion object {
+        const val BASE_URL: String = "https://hp-api.onrender.com"
+        const val API_PRODUCTS: String = "$BASE_URL/api/characters"
+    }
+}
