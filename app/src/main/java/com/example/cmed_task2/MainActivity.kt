f8777b854d384cc8a30403cee1dc1e58b9d6d363
omi-72 <omi@techetronventures.com>
2023-10-26 22:32:41 +0600

    set ViewModel , Adapter & progressDialog as observable

--- app/src/main/java/com/example/cmed_task2/MainActivity.kt
+++ app/src/main/java/com/example/cmed_task2/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.example.cmed_task2
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import com.example.cmed_task2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var progressDialog: Dialog
+    private lateinit var characterAdapter: CharacterAdapter
+
+    private val viewModel: CharacterViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        prepareViews()
+        progressDialog = ProgressDialog(this).progressDialog()
+        viewModel.loadFollowingData()
+
+        viewModel.isDialogueShowing.observe(this, Observer {
+            if (it == true){
+                showProgressDialog()
+            }else{
+                hideProgressDialog()
+            }
+        })
+
+    }
+
+    private fun prepareViews() {
+        TODO("Not yet implemented")
+    }
+
+    private fun showProgressDialog() {
+        this.runOnUiThread {
+            progressDialog.show()
+        }
+    }
+
+    private fun hideProgressDialog() {
+        this.runOnUiThread {
+            progressDialog.dismiss()
+        }
     }
 }
